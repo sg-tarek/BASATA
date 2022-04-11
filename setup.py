a92de80e80c26543e56a4eb94f5afcf148cd3ac1
Ghanoum <tghanoum@deloitte.dk>
2022-04-11 14:36:23 +0200

    bug fixes in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name='basata',
-    version='1.0.0',
+    version='1.0.1',
     description='BASATA - Code less while gaining more',
     long_description=readme(),
     long_description_content_type="text/markdown",
