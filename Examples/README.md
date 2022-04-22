84052aa0e4bb73fbb29ea6083f8b61aacfc77f60
Tarek Ghanoum <ta.ghanoum@gmail.com>
2022-04-22 12:27:32 +0200

    added datasets folder and examples

---
+++ Examples/README.md
@@ -0,0 +1,7 @@
+# Examples Index
+
+|Topic|Notebook Link|
+| :---:|:---:|
+| Titanic Classification | [link](https://github.com/pycaret/pycaret/blob/master/examples/Fetal%20State%20Classification%20Tutorial%20-FSC101.ipynb)|
+
+
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='basata',
-    version='1.0.10',
+    version='1.0.11',
     description='BASATA - Code less while gaining more',
     long_description=readme(),
     long_description_content_type="text/markdown",
